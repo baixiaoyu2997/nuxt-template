872abfdd2acc7ddd53829e1fe0e8557c3900ee5e
白晓宇 <baixiaoyu@yeeyuntech.com>
2021-11-01 17:59:46 +0800

    feat: 修改nuxt指令为nuxi，优化pinia解构方法

--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,18 @@
     {{ user.name }}
     <van-button @click="user.name = '李四'">change name</van-button>
     <van-button @click="user.age++">up</van-button>
-    <van-button @click="() => (app.theme = 'light')">turn off light</van-button>
-    <van-button @click="() => (app.theme = 'dark')">turn off dark</van-button>
-    <div>{{ app.theme }}</div>
+    <van-button @click="() => (theme = 'light')">turn off light</van-button>
+    <van-button @click="() => (theme = 'dark')">turn off dark</van-button>
+    <div>{{ theme }}</div>
   </div>
 </template>
 <script setup>
+
 import useUser from '~/store/user'
 import useApp from '~/store/app'
+
 const user = useUser()
-const app = useApp()
+const {theme} = toRefs(useApp())
 </script>
 
 <style>
--- store/app.js
+++ store/app.js
@@ -1,11 +1,13 @@
 import { defineStore } from 'pinia'
-import { reactive, useNuxtApp,toRefs,toRef } from '#app'
+import { reactive, useNuxtApp, toRef, } from '#app'
 
+// 使用composition-api风格编写的store在使用时，解构必须要用storeToRefs包裹。
 const useApp = defineStore('app', () => {
-  const $colorMode= reactive(useNuxtApp().vue2App.$colorMode)
-  const app = reactive({
-    theme:toRef($colorMode,'preference'),
-  })
-  return toRefs(app)
+  const colorMode=reactive(useNuxtApp().vue2App.$colorMode)
+  const theme = toRef(colorMode,"preference")
+
+  return {
+    theme
+  }
 })
 export default useApp
